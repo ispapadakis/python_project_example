86b42829de064d9277a25ee2b4e0bc367644bfce
Yanni Papadakis <yanni.papadakis@gmail.com>
2024-02-05 20:11:01 -0500

    same format throughout

--- hangman_yp.py
+++ hangman_yp.py
@@ -5,98 +5,107 @@ import random
 wordDictionary = ["sunflower", "house", "diamond", "memes","yeet","hello", "howdy", "like", "subscribe"]
 ### Choose a random word
 randomWord = random.choice(wordDictionary)
-
-
-def print_hangman(wrong):
-  if(wrong == 0):
-    print("\n+---+")
-    print("    |")
-    print("    |")
-    print("    |")
-    print("   ===")
-  elif(wrong == 1): 
-    print("\n+---+")
-    print("O   |")
-    print("    |")
-    print("    |")
-    print("   ===")
-  elif(wrong == 2):
-    print("\n+---+")
-    print("O   |")
-    print("|   |")
-    print("    |")
-    print("   ===")
-  elif(wrong == 3):
-    print("\n+---+")
-    print(" O  |")
-    print("/|  |")
-    print("    |")
-    print("   ===")
-  elif(wrong == 4):
-    print("\n+---+")
-    print(" O  |")
-    print("/|\ |")
-    print("    |")
-    print("   ===")
-  elif(wrong == 5):
-    print("\n+---+")
-    print(" O  |")
-    print("/|\ |")
-    print("/   |")
-    print("   ===")
-  elif(wrong == 6):
-    print("\n+---+")
-    print(" O   |")
-    print("/|\  |")
-    print("/ \  |")
-    print("    ===")
-
-def printGuesses(guess_list):
-  print(*guess_list, sep=" ")
     
 class Hangman:
-  def __init__(self, word):
-    self.word = word
-    print("Welcome to hangman")
-    print("-"*20)
+    def __init__(self, word):
+        self.word = word
+        print("Welcome to hangman")
+        print("-"*20)
 
-  def printWord(self, guessedLetters):
-    allCorrect = True
-    for char in self.word:
-      if char in guessedLetters:
-        print(char, end=" ")
-      else:
-        print("_", end=" ")
-        allCorrect = False
-    print()
-    return allCorrect
+    def printWord(self, guessedLetters):
+        allCorrect = True
+        for char in self.word:
+            if char in guessedLetters:
+                print(char, end=" ")
+            else:
+                print("_", end=" ")
+                allCorrect = False
+        print()
+        return allCorrect
 
-  def play(self):
-    amount_of_times_wrong = 0
-    current_letters_guessed = []
-    allGuessesCorrect = False
+    def printGuesses(self, guess_list):
+        print(*guess_list, sep=" ")
 
-    while True:
-        ### Prompt user for input
-        letterGuessed = input("\nGuess a letter: ")
-        current_letters_guessed.append(letterGuessed)
+    def play(self):
+        amount_of_times_wrong = 0
+        current_letters_guessed = []
+        allGuessesCorrect = False
 
-        ### If Guess is Wrong Increment Error Count
-        if letterGuessed not in self.word:
-            amount_of_times_wrong += 1
+        while True:
+            ### Prompt user for input
+            letterGuessed = input("\nGuess a letter: ")
+            if letterGuessed in current_letters_guessed:
+                print("You guessed this letter earlier. Try again.")
+                continue
+            if len(letterGuessed) > 1:
+                print("Guess one letter at a time. Try again.")
+                continue
+            if not letterGuessed.isalpha():
+                print("You guess needs to be a letter. Try again.")
+                continue
+            current_letters_guessed.append(letterGuessed.lower())
 
-        ### Give Feedback to User
-        print("\nLetters guessed so far: ")
-        printGuesses(current_letters_guessed)
-        print_hangman(amount_of_times_wrong)
-        allGuessesCorrect = self.printWord(current_letters_guessed)
+            ### If Guess is Wrong Increment Error Count
+            if letterGuessed not in self.word:
+                amount_of_times_wrong += 1
 
-        if amount_of_times_wrong >= 6:
-            print("\nYou Lose! Better Luck Next Time.")
-            return
-        if allGuessesCorrect:
-            print("\nCongratulations, You Win!")
-            return
+            ### Give Feedback to User
+            print("\nLetters guessed so far: ")
+            self.printGuesses(current_letters_guessed)
+            self.print_hangman(amount_of_times_wrong)
+            allGuessesCorrect = self.printWord(current_letters_guessed)
 
-game = Hangman(randomWord)
-game.play()
+            if amount_of_times_wrong >= 6:
+                print("\nYou Lose! Better Luck Next Time.")
+                return
+            if allGuessesCorrect:
+                print("\nCongratulations, You Win!")
+                return
+        
+    def print_hangman(self, wrong):
+        if(wrong == 0):
+            print("\n+---+")
+            print("    |")
+            print("    |")
+            print("    |")
+            print("   ===")
+        elif(wrong == 1): 
+            print("\n+---+")
+            print("O   |")
+            print("    |")
+            print("    |")
+            print("   ===")
+        elif(wrong == 2):
+            print("\n+---+")
+            print("O   |")
+            print("|   |")
+            print("    |")
+            print("   ===")
+        elif(wrong == 3):
+            print("\n+---+")
+            print(" O  |")
+            print("/|  |")
+            print("    |")
+            print("   ===")
+        elif(wrong == 4):
+            print("\n+---+")
+            print(" O  |")
+            print("/|\ |")
+            print("    |")
+            print("   ===")
+        elif(wrong == 5):
+            print("\n+---+")
+            print(" O  |")
+            print("/|\ |")
+            print("/   |")
+            print("   ===")
+        elif(wrong == 6):
+            print("\n+---+")
+            print(" O   |")
+            print("/|\  |")
+            print("/ \  |")
+            print("    ===")
+
+if __name__ == "__main__":
+    game = Hangman(randomWord)
+    game.play()
